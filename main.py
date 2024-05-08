ef60836b8356998f1ffb8c29c02996a8f60dd2a2
Soham Sonar <soham.sonar427@gmail.com>
2024-05-07 23:45:54 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import pickle
+import numpy as np
+import cv2
+import face_recognition
+import cvzone
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+import numpy as np
+from datetime import datetime
