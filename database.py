29899ab8784452c3ea2dad08660e4f3a0a9463eb
Soham Sonar <soham.sonar427@gmail.com>
2024-05-07 23:49:21 -0500

    initial commit

---
+++ database.py
@@ -0,0 +1,11 @@
+AddDatatoDatabase.py
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': ""
+})
+
+ref = db.reference('Students')
