d5de90595239baa667afa49cb8b9b1dd46a3914f
Soham Sonar <soham.sonar427@gmail.com>
2024-05-09 23:49:06 -0500

    initialize

--- main.py
+++ main.py
@@ -47,3 +47,14 @@ counter = 0
 id = -1
 imgStudent = []
 
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
